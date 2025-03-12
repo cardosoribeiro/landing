87ec7ebb332a10478a9141772184bedeed7b80ef
cardosoribeiro <cardosoribeiro.mail@gmail.com>
2025-03-12 15:59:03 -0300

    changes CourseBean

--- landing/src/main/java/br/com/landing/beans/CourseBean.java
+++ landing/src/main/java/br/com/landing/beans/CourseBean.java
@@ -16,36 +16,73 @@ import br.com.landing.entity.Department;
 import br.com.landing.repository.Repository;
 import br.com.landing.utils.FacesMessageUtils;
 
+/**
+ * Managed bean for handling Course and Department operations.
+ * This bean is view scoped, meaning it lives as long as the view is active.
+ */
 @ManagedBean(name = "courseBean")
 @ViewScoped
 public class CourseBean implements Serializable {
 
+    /** Entity manager for JPA operations. */
     @PersistenceContext(unitName = "landing-pu")
     private EntityManager entityManager;
+
+    /** Repository for Course entities. */
     private Repository<Course> courseRepository;
+
+    /** Repository for Department entities. */
     private Repository<Department> departmentRepository;
 
+    /** List of courses to be displayed in the view. */
     private List<Course> courses;
+
+    /** List of departments to be displayed in the view. */
     private List<Department> departments;
 
+    /** New Course object for adding or editing courses. */
+    private Course newCourse = new Course();
+
+    /**
+     * Retrieves the list of courses.
+     *
+     * @return List of Course objects.
+     */
     public List<Course> getCourses() {
         return courses;
     }
 
+    /**
+     * Retrieves the list of departments.
+     *
+     * @return List of Department objects.
+     */
     public List<Department> getDepartments() {
         return departments;
     }
 
-    private Course newCourse = new Course();
-
+    /**
+     * Retrieves the new Course object.
+     *
+     * @return Course object.
+     */
     public Course getNewCourse() {
         return newCourse;
     }
 
+    /**
+     * Sets the new Course object.
+     *
+     * @param newCourse Course object to be set.
+     */
     public void setNewCourse(Course newCourse) {
         this.newCourse = newCourse;
     }
 
+    /**
+     * Tests if the entity manager is initialized.
+     * Prints a message to the console indicating the status.
+     */
     private void testEntityManager() {
         if (entityManager == null) {
             System.out.println("EntityManager is NULL!");
@@ -54,10 +91,14 @@ public class CourseBean implements Serializable {
         }
     }
 
+    /**
+     * Initializes the bean.
+     * Creates the repositories and loads the courses and departments.
+     */
     @PostConstruct
     private void init() {
         try {
-            EntityManagerFactory emf = Persistence.createEntityManagerFactory("landing-pu"); 
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("landing-pu");
             entityManager = emf.createEntityManager();
 
             courseRepository = new Repository<>(entityManager, Course.class);
@@ -70,6 +111,9 @@ public class CourseBean implements Serializable {
         }
     }
 
+    /**
+     * Loads the departments from the database.
+     */
     private void loadDepartments() {
         try {
             departments = departmentRepository.findAll();
@@ -79,6 +123,9 @@ public class CourseBean implements Serializable {
         }
     }
 
+    /**
+     * Loads the courses from the database.
+     */
     private void loadCourses() {
         try {
             courses = courseRepository.findAll();
@@ -88,6 +135,11 @@ public class CourseBean implements Serializable {
         }
     }
 
+    /**
+     * Adds a new course to the database.
+     *
+     * @return Navigation outcome.
+     */
     public String addCourse() {
         try {
             Department selectedDepartment = departmentRepository.findById(newCourse.getDepartment().getDepartmentName());
@@ -108,6 +160,11 @@ public class CourseBean implements Serializable {
         }
     }
 
+    /**
+     * Loads the course for editing based on the request parameter.
+     *
+     * @param event Component system event.
+     */
     public void loadCourseForEdit(ComponentSystemEvent event) {
         FacesContext context = FacesContext.getCurrentInstance();
         Map<String, String> params = context.getExternalContext().getRequestParameterMap();
@@ -120,7 +177,10 @@ public class CourseBean implements Serializable {
         }
     }
 
-    public void editCourse() {        
+    /**
+     * Edits the course loaded in newCourse.
+     */
+    public void editCourse() {
         if (newCourse.getCourseId() != null) {
             newCourse = courseRepository.findById(newCourse.getCourseId());
             if (newCourse == null) {
@@ -132,7 +192,12 @@ public class CourseBean implements Serializable {
         }
     }
 
-    public String updateCourse() {        
+    /**
+     * Updates the course in the database.
+     *
+     * @return Navigation outcome.
+     */
+    public String updateCourse() {
         try {
             Department selectedDepartment = departmentRepository.findById(newCourse.getDepartment().getDepartmentName());
             if (selectedDepartment == null) {
@@ -152,6 +217,11 @@ public class CourseBean implements Serializable {
         }
     }
 
+    /**
+     * Deletes the course from the database.
+     *
+     * @param courseId Course ID to be deleted.
+     */
     public void deleteCourse(String courseId) {
         try {
             Course course = courseRepository.findById(courseId);
